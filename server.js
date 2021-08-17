e28a29f4f7e7abe3d62e7a9afb9986f6f702c4f8
Kyle Mitchell <kdavidmitchell@gmail.com>
2021-08-17 12:43:55 -0700

    adding debugger library

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const keys = require('./config/keys.js');
+const compression = require('compression');
+const helmet = require('helmet');
 const app = express();
 const bodyParser = require('body-parser');
 
-// parse application/x-www-form-urlencoded
+// Set up middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(compression());
+app.use(helmet());
 
 // Setting up DB
 const mongoose = require('mongoose');
 mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
+
 // Setup database models
 require('./model/Account');
 require('./model/PlayTrace');
@@ -17,10 +22,13 @@ require('./model/LevelTrace');
 require('./model/DayTrace');
 require('./model/JournalTrace');
 
+// Setup debugger
+const debugInit = require('./debuggers.js').init;
+
 // Setup the routes
 require('./routes/authenticationRoutes')(app);
 require('./routes/traceRoutes')(app);
 
 app.listen(keys.port, () => {
-    console.log("Listening on " + keys.port);
+    debugInit("Listening on " + keys.port);
 });
