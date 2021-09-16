73fe7eebed4b03a3eadeac0d58676ca1c826f6f8
Kyle Mitchell <kdavidmitchell@gmail.com>
2021-09-16 10:01:49 -0700

    adding traces to website homepage

--- routes/index.js
+++ routes/index.js
@@ -11,31 +11,51 @@ module.exports = app => {
   /* GET home page. */
   app.get('/', function(req, res, next) {
     PlayTrace.find({}, function(err, playtraces) {
-        if (err)
-          res.send(err);
-        LevelTrace.find({}, function(err, leveltraces) {
-          if (err)
-            res.send(err);
-          DayTrace.find({}, function(err, daytraces) {
-            if (err)
-              res.send(err);
+      if (err) res.send(err);
+      LevelTrace.find({}, function(err, leveltraces) {
+        if (err) res.send(err);
+        DayTrace.find({}, function(err, daytraces) {
+          if (err)res.send(err);
         res.render('index', { title: 'Space Zoologist Database', playtraces: playtraces, leveltraces: leveltraces, daytraces: daytraces });
         });
       });
     });
   });
 
-  /* GET export route. */
-  app.get('/export', function(req, res, next) {
+  /* GET export routes. */
+  app.get('/exportplaytraces', function(req, res, next) {
     var filename = "playtraces.csv";
-    var dataArray;
     PlayTrace.find().lean().exec({}, function(err, playtraces) {
-        if (err) res.send(err);
-        
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/csv');
-        res.setHeader("Content-Disposition", 'attachment; filename='+filename);
-        res.csv(playtraces, true);
+      if (err) res.send(err);
+  
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/csv');
+      res.setHeader("Content-Disposition", 'attachment; filename='+filename);
+      res.csv(playtraces, true);
+    });
+  });
+
+  app.get('/exportleveltraces', function(req, res, next) {
+    var filename = "leveltraces.csv";
+    LevelTrace.find().lean().exec({}, function(err, leveltraces) {
+      if (err) res.send(err);
+  
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/csv');
+      res.setHeader("Content-Disposition", 'attachment; filename='+filename);
+      res.csv(leveltraces, true);
+    });
+  });
+
+  app.get('/exportdaytraces', function(req, res, next) {
+    var filename = "daytraces.csv";
+    DayTrace.find().lean().exec({}, function(err, daytraces) {
+      if (err) res.send(err);
+  
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/csv');
+      res.setHeader("Content-Disposition", 'attachment; filename='+filename);
+      res.csv(daytraces, true);
     });
   });
 }
